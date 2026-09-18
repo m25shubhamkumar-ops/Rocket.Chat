d607f4174988f8b4a984a753446720c624f83698
Ricardo Garim <rswarovsky@gmail.com>
2026-09-18 13:57:55 -0300

    test: improve Upload service coverage

--- apps/meteor/jest.config.ts
+++ apps/meteor/jest.config.ts
@@ -45,6 +45,7 @@ export default {
 				'<rootDir>/app/utils/lib/**.spec.ts',
 				'<rootDir>/server/lib/auditServerEvents/**.spec.ts',
 				'<rootDir>/server/services/import/**/*.spec.ts',
+				'<rootDir>/server/services/upload/*.spec.ts',
 				'<rootDir>/server/settings/lib/**.spec.ts',
 				'<rootDir>/server/startup/migrations/**.spec.ts',
 				'<rootDir>/server/cron/**.spec.ts',
